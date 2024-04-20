0c4b903d11d6872442698869db92b2f230a5b769
Said Rodrigues <coderflemis@gmail.com>
2024-04-19 21:53:12 -0300

    improvement(#53): update register-form to be more test friendly

--- frontend/src/components/pages/signup/register-section.tsx
+++ frontend/src/components/pages/signup/register-section.tsx
@@ -1,9 +1,17 @@
+"use client";
+
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import RegisterForm from "./register-form";
+import RegisterForm, { RegisterFormSchema } from "./register-form";
 
 function RegisterSection() {
+  async function submitForm(v: RegisterFormSchema) {
+    await new Promise((resolve) => setTimeout(resolve, 0));
+    console.log(v);
+    return true;
+  }
+
   return (
     <section className="col-span-4 lg:col-span-2 justify-self-center flex flex-col w-full py-12 px-4 gap-8 max-w-lg">
       <Image
@@ -22,7 +30,7 @@ function RegisterSection() {
           Digite suas credenciais
         </h3>
       </div>
-      <RegisterForm />
+      <RegisterForm submitForm={submitForm} />
       <p className="text-sm text-foreground">
         Ja possui uma conta?
         <Button variant="link" size="sm" asChild>
