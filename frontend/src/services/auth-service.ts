50cb7fbcd5f2c458930c722f3fe9e802f1ae3175
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-04-19 23:36:23 -0300

    feature(#52): add auth service

---
+++ frontend/src/services/auth-service.ts
@@ -0,0 +1,81 @@
+import apiProvider from "@/providers/api-provider";
+
+type SignUpRequest = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+type SignInRequest = {
+  username: string;
+  password: string;
+};
+
+type RefreshRequest = {
+  refresh_token: string;
+}
+
+type SignUpResponse = {
+  access_token: string;
+  refresh_token: string;
+  expire_at: string;
+};
+
+type SignInResponse = SignUpResponse;
+
+type RefreshResponse = SignUpResponse;
+
+type UserInfoResponse = {
+  id: string;
+  username: string;
+  email: string;
+}
+
+const signUp = async (data: SignUpRequest): Promise<SignUpResponse> => {
+  try {
+    const response = await apiProvider.usePost<SignUpResponse, SignUpRequest>("/auth/register", data)
+
+    return response;
+  } catch (error) {
+    throw error; 
+  }
+}
+
+const signIn = async (data: SignInRequest): Promise<SignUpResponse> => {
+  try {
+    const response = await apiProvider.usePost<SignInResponse, SignInRequest>("/auth/", data)
+
+    return response;
+  } catch (error) {
+    throw error; 
+  }
+}
+
+const refreshToken = async (data: RefreshRequest): Promise<RefreshResponse> => {
+  try {
+    const response = await apiProvider.usePost<RefreshResponse, RefreshRequest>("/auth/refresh", data)
+
+    return response;
+  } catch (error) {
+    throw error; 
+  }
+}
+
+const getUserInfo = async (): Promise<UserInfoResponse> => {
+  try {
+    const response = await apiProvider.useGet<UserInfoResponse>("/auth/userinfo")
+
+    return response;
+  } catch (error) {
+    throw error; 
+  }
+}
+
+const authService = {
+  signIn,
+  signUp,
+  refreshToken,
+  getUserInfo
+}
+
+export default authService;
