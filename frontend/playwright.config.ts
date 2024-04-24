04150dcf5d13e372c41b449ccd7ce70423b4cec7
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-04-24 17:49:47 -0300

    tests(#52): update worker config in playwright

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
     baseURL: baseURL,
