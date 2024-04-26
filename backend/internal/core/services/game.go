912926dc1465e2514c6c42eb378795a43cb4d069
Said Rodrigues <coderflemis@gmail.com>
2024-04-26 14:25:33 -0300

    feature(#58): add game service

---
+++ backend/internal/core/services/game.go
@@ -0,0 +1,51 @@
+package services
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	"github.com/taldoflemis/brain.test/internal/core/domain/game_aggregate"
+	"github.com/taldoflemis/brain.test/internal/ports"
+)
+
+type GameService struct {
+	logger            ports.Logger
+	validationService *ValidationService
+	gameStorer        ports.GameStorer
+}
+
+func NewGameService(
+	logger ports.Logger,
+	validationService *ValidationService,
+	gameStorer ports.GameStorer,
+) *GameService {
+	return &GameService{
+		logger:            logger,
+		validationService: validationService,
+		gameStorer:        gameStorer,
+	}
+}
+
+func (s *GameService) CreateNewGame(
+	ctx context.Context,
+	userId string,
+	req *game.Game,
+) error {
+	req.OwnerId = userId
+	req.Id = uuid.New()
+
+	err := s.validationService.Validate(req)
+	if err != nil {
+		s.logger.Errorf("Failed to validate game %v", err)
+		return err
+	}
+
+	err = s.gameStorer.StoreGame(ctx, req)
+	if err != nil {
+		s.logger.Errorf("Failed to store game %v", err)
+		return err
+	}
+
+	return nil
+}
