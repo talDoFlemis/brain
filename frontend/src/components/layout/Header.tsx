252e3514115cdcda6a36aec7316a7d45d4a85648
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-05-28 23:18:16 -0300

    improvement(#43): Add signout button to Header

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,17 +1,20 @@
-import { authOptions } from "@/providers/auth";
-import authService from "@/services/auth-service";
-import { getServerSession } from "next-auth";
+"use client";
 import { IoLogOutOutline } from "react-icons/io5";
 import { Skeleton } from "@/components/ui/skeleton";
+import { signOut } from "next-auth/react";
+import useSessionWithRefresh from "@/hooks/useSessionWithRefresh";
 
-async function Header() {
-  const session = await getServerSession(authOptions);
-  const user = await authService.getUserInfo(session?.access_token ?? "");
+function Header() {
+  const { data } = useSessionWithRefresh();
+
   return (
     <header className="flex justify-end h-16 px-6 w-full bg-background">
       <div className="flex items-center gap-8 w-fit bg-foreground text-secondary-foreground p-4 rounded-lg">
-        <span>{user.username}</span>
-        <IoLogOutOutline className="w-6 h-6" />
+        <span>{data && data.user.username}</span>
+        <IoLogOutOutline
+          className="w-6 h-6 cursor-pointer transition-colors duration-200 ease-in hover:text-accent"
+          onClick={() => signOut()}
+        />
       </div>
     </header>
   );
