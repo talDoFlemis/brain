ffdc4db08a56478171c5f1262cb19dad81cab29d
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2024-05-28 23:09:16 -0300

    improvement(#43): Add Game listing to dashboard

--- frontend/src/components/pages/dashboard/created-quizes-section.tsx
+++ frontend/src/components/pages/dashboard/created-quizes-section.tsx
@@ -1,53 +1,47 @@
-import { FaEdit } from "react-icons/fa";
-import { FaPlay } from "react-icons/fa";
+"use client";
 
-type QuizCardProps = {
-  title: string;
-  body: string;
-};
-
-function QuizCard({ title, body }: QuizCardProps) {
-  return (
-    <div className="flex flex-col bg-foreground text-secondary-foreground h-80 w-96 gap-4 p-4 rounded-lg overflow-y-auto">
-      <h3 className="text-2xl font-bold">{title}</h3>
-      <p className="font-light leading-6">{body}</p>
-      <div className="mt-auto flex grow justify-start gap-8 py-8">
-        <FaEdit className="w-8 h-8 cursor-pointer" />
-        <FaPlay className="w-8 h-8 text-accent cursor-pointer" />
-      </div>
-    </div>
-  );
-}
+import { useEffect, useState } from "react";
+import { QuizCard } from "./quiz-card";
+import gameService, { Game } from "@/services/game-service";
+import useSessionWithRefresh from "@/hooks/useSessionWithRefresh";
+import Loading from "@/components/common/Loading";
 
 function CreateQuizesSection() {
+  const [games, setGames] = useState<Game[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const { data } = useSessionWithRefresh();
+
+  useEffect(() => {
+    if (!data) return;
+    gameService
+      .getGamesByUser(data.access_token)
+      .then((response) => setGames(response.games))
+      .catch((error) => {
+        console.log(error);
+        setGames([]);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [data]);
+
+  const content =
+    games.length === 0 ? (
+      <h2 className="text-white/80 text-2xl">Nenhum quiz criado no momento</h2>
+    ) : (
+      games.map((game) => {
+        return (
+          <QuizCard key={game.id} title={game.title} body={game.description} />
+        );
+      })
+    );
+
   return (
     <section className="flex flex-col gap-8 h-screen w-full px-4 overflow-y-hidden">
       <h1 className="text-4xl text-secondary-foreground">Quizzes criados</h1>
       <section className="flex flex-wrap grow w-full gap-4 overflow-y-auto">
-        <QuizCard
-          title="Test quiz 01"
-          body="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia."
-        />
-        <QuizCard
-          title="Test quiz 01"
-          body="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia."
-        />
-        <QuizCard
-          title="Test quiz 01"
-          body="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia."
-        />
-        <QuizCard
-          title="Test quiz 01"
-          body="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia."
-        />
-        <QuizCard
-          title="Test quiz 01"
-          body="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia."
-        />
-        <QuizCard
-          title="Test quiz 01"
-          body="Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia."
-        />
+        {loading ? <Loading /> : content}
       </section>
     </section>
   );
